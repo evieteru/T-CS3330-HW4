0ccdb7ad20da9e9fc89dc7fb05cf14def65cd8c7
evieteru <eviezushi@Nod>
2024-02-18 02:21:57 -0600

    Addition of two initial class files, method signatures specified in assignmnet sheet, and the csv file to be read from.

---
+++ src/hw4/VehicleManager.java
@@ -0,0 +1,142 @@
+package hw4;
+
+public class VehicleManager {
+	
+	public boolean readFromFile(String fileName) {
+		
+	}
+	
+	
+	public void VehicleManager(String fileName) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	//Displays
+	public void displayAllCarInformation() {
+		
+	}
+	
+	
+	public void displayAllTruckInformation() {
+		
+	}
+	
+	public void displayAllSUVInformation() {
+		
+	}
+	
+	
+	public void displayAllMotorBikeInformation() {
+		
+	}
+	
+	
+	public void displayVehicleInformation(Vehicle v) {
+		
+	}
+	
+	
+	public void displayAllVehicleInformation() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//General
+	
+	public boolean removeVehicle(Vehicle vehicle) {
+		
+	}
+	
+	
+	public boolean addVehicle(Vehicle vehicle) {
+		
+	}
+	
+	public boolean saveVehicleList(){
+			
+		}
+	
+	
+	private boolean isVehicleType(Vehicle v, Class clazz) {
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Vehicle Search
+	public int getNumberOfVehichlesByType(Class clazz){
+		
+	}
+	
+	public Vehicle getVehicleWithHighestMaintenanceCost(double
+		distance) {
+		
+	}
+	
+	
+	public Vehicle getVehicleWithLowestMaintenanceCost(double
+		distance){
+			
+		}
+	
+	
+	public ArrayList<Vehicle>
+		getVehicleWithHighestFuelEfficiency(double distance, double
+		fuelPrice){
+			
+		}
+	
+	
+	public ArrayList<Vehicle>
+		getVehicleWithLowestFuelEfficiency(double distance, double
+		fuelPrice){
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Miscellaneous
+	public double getAverageFuelEfficiencyOfSUVs(double distance,
+		double fuelPrice) {
+			
+		}
+		
+
+}
